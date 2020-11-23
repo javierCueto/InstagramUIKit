902808052df8bf98c1a2aaab504fe3685080df0b
javierCueto <javelie@outlook.com>
2020-11-23 09:54:50 -0700

    configure project

---
+++ InstagramUIKit/Controller/MainTabController.swift
@@ -0,0 +1,15 @@
+//
+//  MainTabController.swift
+//  InstagramUIKit
+//
+//  Created by Javier Cueto on 23/11/20.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
