d75cb8b4c8aa21ae69833fad1696fcf7e3698c39
javierCueto <javelie@outlook.com>
2020-11-23 19:55:23 -0700

    Login Page UI

---
+++ InstagramUIKit/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,14 @@
+//
+//  RegistrationController.swift
+//  InstagramUIKit
+//
+//  Created by Javier Cueto on 23/11/20.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
