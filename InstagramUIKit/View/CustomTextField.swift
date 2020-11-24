05f7c0b7ff814dad5c6288d33916bb3ac94235c5
javierCueto <javelie@outlook.com>
2020-11-24 09:25:02 -0700

    Creating custom subclasses for reusability - Subclass for textfield and extension for button text

---
+++ InstagramUIKit/View/CustomTextField.swift
@@ -0,0 +1,31 @@
+//
+//  CustomTextField.swift
+//  InstagramUIKit
+//
+//  Created by Javier Cueto on 24/11/20.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+    init(placeholder: String) {
+        super.init(frame: .zero)
+        
+        let spacer = UIView()
+        spacer.setDimensions(height: 50, width: 12)
+        leftView = spacer
+        leftViewMode = .always
+        
+        borderStyle = .none
+        textColor = .white
+        keyboardAppearance = .dark
+        keyboardType = .default
+        backgroundColor = UIColor(white: 1, alpha: 0.1)
+        setHeight(50)
+        attributedPlaceholder = NSAttributedString(string: placeholder, attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
