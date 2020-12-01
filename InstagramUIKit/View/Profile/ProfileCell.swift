94c7b976549fa76c9f7dcf3822e550e5db7b2513
javierCueto <javelie@outlook.com>
2020-11-30 19:48:22 -0700

    finishing up profile UI

--- InstagramUIKit/View/Profile/ProfileCell.swift
+++ InstagramUIKit/View/Profile/ProfileCell.swift
@@ -10,12 +10,21 @@ import UIKit
 class ProfileCell: UICollectionViewCell {
     
     // MARK: -  PROPERTIES
+    private let postImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = #imageLiteral(resourceName: "venom-7")
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+    }()
     
     // MARK: -  LIFECYCLE
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .black
-        myPrint("making cells")
+        
+        addSubview(postImageView)
+        postImageView.fillSuperview()
     }
     
     required init?(coder: NSCoder) {
